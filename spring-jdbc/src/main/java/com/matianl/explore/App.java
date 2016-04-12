441d56c31b9971a675456fe13e57df253bb50e96
matinalhope <matinal.hope@gmail.com>
2016-04-12 17:13:32 +0800

    调整App，使之不需要try。之前会提示ctx未关闭的警告。

--- spring-jdbc/src/main/java/com/matianl/explore/App.java
+++ spring-jdbc/src/main/java/com/matianl/explore/App.java
@@ -6,23 +6,26 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.matianl.explore.service.AdminPermissionService;
 
 /**
  * Hello world!
- *
  */
 public class App {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(App.class);
-	
+
 	public static void main(String[] args) {
-		try(ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-conf/applicationContext.xml")){
-			AdminPermissionService bean2 = ctx.getBean(AdminPermissionService.class);
-			List<Map<String, Object>> list = bean2.list(Arrays.asList(-1));
-			log.info("list result: {}", list);
-		}
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("spring-conf/applicationContext.xml");
+		testList(ctx);
+	}
+
+	private static void testList(AbstractApplicationContext ctx) {
+		AdminPermissionService bean2 = ctx.getBean(AdminPermissionService.class);
+		List<Map<String, Object>> list = bean2.list(Arrays.asList(-1));
+		log.info("list result: {}", list);
 	}
 }
